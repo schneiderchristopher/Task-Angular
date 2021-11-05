f00131d4735172d27d31b4c50bc230a42a0bf3cb
Christopher Schneider <schneiderchristopher16@gmail.com>
2021-11-05 13:06:08 -0300

    commitando all

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from 'src/app/Task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.scss']
+})
+export class TasksComponent implements OnInit {
+
+  tasks!: Task[];
+  
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
+  }
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((newTask) => {
+      this.tasks.push(newTask);
+    });
+  }
+
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => {
+      this.tasks = this.tasks.filter((t) => t.id !== task.id);
+    });
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+}
